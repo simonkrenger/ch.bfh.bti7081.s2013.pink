db1353080dbc2662f514bc3ef527c9b4a0852771
Marco Berger <lostchall@gmail.com>
2013-05-20 15:35:50 +0200

    added some patientoverview-elements

--- Project Pink/src/main/java/ch/bfh/bti7081/s2013/pink/SessionOverView.java
+++ Project Pink/src/main/java/ch/bfh/bti7081/s2013/pink/SessionOverView.java
@@ -14,13 +14,13 @@
  * the License.
  */
 package ch.bfh.bti7081.s2013.pink;
-
 import ch.bfh.bti7081.s2013.pink.model.Patient;
 import ch.bfh.bti7081.s2013.pink.model.Session;
 import ch.bfh.bti7081.s2013.pink.model.TestDataSource;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 
 /**
@@ -32,24 +32,34 @@ public class SessionOverView extends VerticalLayout implements View {
 	private int posY;
 	private Session session;
 	private Patient patient;
+	private Label headerTitle;
 	private TestDataSource testenvironment;
 
 	public SessionOverView() {
 		session = testenvironment.getSession();
 		patient = session.getPatient();
 		patient = testenvironment.getPatient();
+		posX = 0;
 
 		setSizeFull();
+		patientSearch();
 		showPatients();
 	}
 
 	public void showPatients() {
 		// loop trough the next 3 patients
 		for (int i = 0; i < 3; i++) {
-			PatientOverview patientOverview = new PatientOverview(posX, 20,
+			PatientOverview patientOverview = new PatientOverview(posX, 200,
 					patient, session);
 			addComponent(patientOverview);
 		}
+	}
+
+	public void patientSearch() {
+
+		PatientSearchView patientSearchView = new PatientSearchView(patient);
+
+		addComponent(patientSearchView);
 	}
 
 	@Override
